be23af83197eadabc9dc0b70384c0d3f4435a2f6
Markus Goessling <markus_goessling@gmx.net>
2021-04-21 22:42:28 +0200

    created alias.sh

---
+++ alias.sh
@@ -0,0 +1,8 @@
+#
+# Script zum setzen von nützlichen Aliasen
+# Aufruf: ./alias.sh 
+#_____________________________________________________
+# 18.08.2006	design		Markus Goessling 
+#
+#######################################################
+alias wetter curl http://wttr.in
